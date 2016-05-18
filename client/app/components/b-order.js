f6de35f2c7c4ea97e00709714ddf30705ca94901
octocrux <cate.petrusevich@gmail.com>
2016-05-19 01:24:30 +0300

    Add checkout form to b-order component

--- client/app/components/b-order.js
+++ client/app/components/b-order.js
@@ -6,12 +6,18 @@ export default Ember.Component.extend({
   groupedPortions: Ember.computed('nonDeletedPortions', function() {
     return groupBy(this.get('nonDeletedPortions'), 'owner.id');
   }),
+  notifications: Ember.inject.service(),
 
   actions: {
     toggleActiveState() {
       const order = this.get('order');
       order.toggleProperty('active');
       order.save();
+    },
+
+    send(message, order) {
+      Ember.Logger.log(message, order);
+      this.get('notifications').sendOrderNotification(message, order.id);
     }
   }
 });
