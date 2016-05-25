6152f23e4bf78ac70132ffb804804f892dae40d2
octocrux <cate.petrusevich@gmail.com>
2016-05-25 13:46:16 +0300

    Send simple notification to slack

--- client/app/controllers/new-order.js
+++ client/app/controllers/new-order.js
@@ -1,13 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  createNotification(order) {
+    const message = 'New order created!';
+
+    const notification = this.store.createRecord('notification');
+    notification.set('order', order);
+    notification.set('text', message);
+    notification.save();
+  },
+
   actions: {
     createOrder(vendor, account, attrs) {
       const order = this.store.createRecord('order', attrs);
       order.set('vendor', vendor);
       order.set('manager', account);
       order.set('money.required', vendor.get('minOrderCost'));
-      order.save();
+      order.save().then(() => {
+        this.createNotification(order);
+      });
+
       this.transitionToRoute('orders');
     }
   }
