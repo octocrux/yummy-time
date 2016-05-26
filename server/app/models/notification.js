c18d35ea8d5d6b569a762ee4054617ad04ff4f7f
octocrux <cate.petrusevich@gmail.com>
2016-05-26 23:16:20 +0300

    Check account presence before notification save

--- server/app/models/notification.js
+++ server/app/models/notification.js
@@ -2,6 +2,7 @@
 
 const Slack = require('node-slack');
 const mongoose = require('mongoose');
+const Account = require('./account');
 const Schema = mongoose.Schema;
 
 const notificationSchema = new Schema({
@@ -11,6 +12,17 @@ const notificationSchema = new Schema({
 });
 
 notificationSchema.pre('save', function(next) {
+  Account.findOne({ _id: this.manager })
+  .exec((err, manager) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log('Account not found');
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Manager name: ', manager.name);
+    }
+  });
+
   const slack = new Slack(process.env.SLACK_WEBHOOK);
 
   slack.send({
