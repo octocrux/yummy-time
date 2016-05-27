e60366550c0a3a6f7581ddcb08ab47152d97c059
octocrux <cate.petrusevich@gmail.com>
2016-05-27 18:42:16 +0300

    Add default (empty) command behaviour

--- server/app/controllers/slack/slack-commands.js
+++ server/app/controllers/slack/slack-commands.js
@@ -3,28 +3,26 @@
 const Commands = require('./commands');
 
 function extractCommand(text) {
+  // TODO: refactor code
   if (!text) {
-    return 'Error in command text';
-  }
-
-  if (text.includes('help')) {
+    return Commands.orders();
+  } else if (text.includes('help')) {
     return Commands.help();
-  }
-  if (text.includes('orders')) {
+  } else if (text.includes('orders')) {
     return Commands.orders();
   }
-};
+  return Commands.error();
+}
 
 exports.callback = function(req, res) {
   if (!req.body) {
     // eslint-disable-next-line no-console
     console.log('Error on parsing request body');
-    res.write('Could not parse request body');
-    res.end();
+    res.send('Could not parse request body');
   } else {
     // eslint-disable-next-line no-console
     console.log(req.body);
-    let command = extractCommand(req.body.text);
+    const command = extractCommand(req.body.text);
     // eslint-disable-next-line no-console
     console.log(command);
     res.send(command);
